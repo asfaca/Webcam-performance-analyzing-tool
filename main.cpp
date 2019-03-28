22f72f4038ef9da61224a5690e0c28268db74541
Suwan Kim <asfaca@naver.com>
2019-03-28 16:28:31 +0900

    Takes arguments for video resolution, camera device id and the number of frame to capture.

--- main.cpp
+++ main.cpp
@@ -11,22 +11,49 @@ using namespace chrono;
 
 int main(int argc, char **argv)
 {
-        int CAM_ID = 0;
-        unsigned int frame_no = 0;
-        unsigned int frame_end = 300;
-        double width = 1920;
-        double height = 1080;
+        int camdev_id;
+        int frame_no = 0;
+        int frame_end;
+        double width, height;
         double fps;
         Mat frame1;
 
-        VideoCapture cap(CAM_ID);
+        if (argc < 5) {
+                perror("Too small input arguments\n"
+                        "Insert [resolution_width] [resolution_height] [camdev_id] [frame_no]\n"
+                        "Resolution width : ex, 4096 for 4K video\n"
+                        "Resolution height : ex, 2160 for 4K video\n"
+                        "Camdev_id : camera device id. path=/dev/video[N] (N is id)\n"
+                        "Frame_no : the number of frame to capture\n");
+                return 0;
+        } else if (argc > 5) {
+                perror("Too many input arguments.\n"
+                        "Insert [resolution_width] [resolution_height] [camdev_id] [frame_no]\n"
+                        "Resolution width : ex, 4096 for 4K video\n"
+                        "Resolution height : ex, 2160 for 4K video\n"
+                        "Camdev_id : camera device id. path=/dev/video[N] (N is id)\n"
+                        "Frame_no : the number of frame to capture\n");
+                return 0;
+        }
+
+        width = stod(argv[1]);
+        height = stod(argv[2]);
+        camdev_id = stoi(argv[3]);
+        frame_end = stoi(argv[4]);
+
+        VideoCapture cap(camdev_id);
         if (!cap.isOpened()) {
                 perror("Can't open the CAM\n");
-                return -1;
+                return 0;
         }
 
         cap.set(CV_CAP_PROP_FRAME_WIDTH, width);
         cap.set(CV_CAP_PROP_FRAME_HEIGHT, height);
+
+	width = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+	height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+	cout << "width: " << width << "\n";
+	cout << "height: " << height << "\n";
 
         auto start = system_clock::now();
         while(1) {
@@ -44,6 +71,6 @@ int main(int argc, char **argv)
 
         fps = frame_end / (double)elapsed_seconds.count();
         cout << "FPS: " << fps << "\n";
-
+	
         return 0;
 }
